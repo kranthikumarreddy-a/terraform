356d929e23829c408623188ca2fbf8914f63b629
kranthi <kranthireddy152@gmail.com>
2026-05-17 17:13:52 +0530

    mongo

---
+++ for_each/r53.tf
@@ -0,0 +1,11 @@
+resource "aws_route53_record" "records" {
+  for_each = aws_instance.example   # ✅ valid, this will work as already instances got created in the ec2.tf file using for_each loop, 
+                                   # if we use count while creating instances in ec.tf, it won't work
+
+  zone_id = var.zone_id
+  name    = "${each.key}.${var.domain_name}"
+  type    = "A"
+  ttl     = 300
+
+  records = [each.value.private_ip]
+}
--- for_each/variables.tf
+++ for_each/variables.tf
@@ -5,3 +5,12 @@ variable "instances" {
     mysql   = "t3.small"
   }
 }
+
+# replace with your zone_id and domain_name
+variable "zone_id" {
+    default = "Z083660518ZYEU99DYFIK"
+}
+
+variable "domain_name" {
+    default = "daws-88sbatch.online"
+}
