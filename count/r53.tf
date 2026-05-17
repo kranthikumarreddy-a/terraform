d22380ec70370683160b4c6db1598d79a969d953
kranthi <kranthireddy152@gmail.com>
2026-05-17 16:05:05 +0530

    mongo

--- count/r53.tf
+++ count/r53.tf
@@ -1,10 +1,10 @@
 resource "aws_route53_record" "records" {
-  count = length(local.private_ips)
+  count = length(var.instances)
 
   zone_id = var.zone_id
-  name    = "${count.index}.${var.domain_name}"
+  name    = "${var.instances[count.index]}.${var.domain_name}"
   type    = "A"
   ttl     = 300
 
-  records = [local.private_ips[count.index]]
+  records = [aws_instance.example[count.index].private_ip]
 }
