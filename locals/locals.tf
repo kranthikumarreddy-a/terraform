8f20bc3a9a9664972e40c795925fdae1e60c0c73
kranthi <kranthireddy152@gmail.com>
2026-05-18 22:02:23 +0530

    mongo

--- locals/locals.tf
+++ locals/locals.tf
@@ -7,6 +7,10 @@ locals {
         Terraform = "true"
     }
 
+    
+  
+
+
     ec2_final_tags = merge(local.common_tags, var.ec2_tags)
     ami_id = data.aws_ami.joindevops.id
 }
--- locals/variables.tf
+++ locals/variables.tf
@@ -1,17 +1,17 @@
 variable "name" {
-    type = string
-    default = "locals"
+  type    = string
+  default = "locals"
 }
-
 
 variable "environment" {
-     type = string
-     default = "dev"
+  type    = string
+  default = "dev"
 }
 
-ec2_tags = {
-
+variable "ec2_tags" {
+  default = {
     Name        = "web-server-1"
     Environment = "dev"
     Owner       = "kranthi"
   }
+}
