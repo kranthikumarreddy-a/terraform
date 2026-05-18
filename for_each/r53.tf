1723e0a60265312b6174dc846cf5b4515d2cbffc
kranthi <kranthireddy152@gmail.com>
2026-05-18 08:14:06 +0530

    mongo

--- for_each/r53.tf
+++ for_each/r53.tf
@@ -1,6 +1,7 @@
 resource "aws_route53_record" "records" {
-  for_each = aws_instance.example   # ✅ valid, this will work as already instances got created in the ec2.tf file using for_each loop, 
-                                   # if we use count while creating instances in ec.tf, it won't work
+  for_each = aws_instance.example  
+   # ✅ valid, this will work as already instances got created in the ec2.tf file using for_each loop, 
+ # if we use count while creating instances in ec.tf, it won't work
 
   zone_id = var.zone_id
   name    = "${each.key}.${var.domain_name}"
